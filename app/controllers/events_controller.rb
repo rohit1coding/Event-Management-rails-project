e951074815bfe68befab377998994ea5f4aedf3b
rohit1coding <rohit1coding@gmail.com>
2022-05-20 02:02:55 +0530

    Delete Event will delete all tasks and expenses associated with it

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -9,60 +9,64 @@ class EventsController < ApplicationController
     @event = Event.new
   end
   
-    def create
-      @event = current_user.events.new(event_params)
-      if @event.save
-        flash[:notice] = "Event #{@event.name} successfully created"
-        redirect_to @event
-      else
-        flash[:alert] = "Something went wrong"
-        render 'new'
-      end
+  def create
+    @event = current_user.events.new(event_params)
+    if @event.save
+      flash[:notice] = "Event #{@event.name} successfully created"
+      redirect_to @event
+    else
+      flash[:alert] = "Something went wrong"
+      render 'new'
     end
-    
-    def edit
-      current_event
+  end
+  
+  def edit
+    current_event
+  end
+  
+  def update
+    current_event
+    if @event.update(event_params)
+      flash[:notice] = "Event successfully Updated!"
+      redirect_to @event
+    else
+      flash[:alert] = "Something went wrong"
+      render 'edit'
     end
-    
-    def update
-      current_event
-      if @event.update(event_params)
-        flash[:notice] = "Event successfully Updated!"
-        redirect_to @event
-      else
-        flash[:alert] = "Something went wrong"
-        render 'edit'
-      end
-    end
-    
-    def show
-      current_event
-      @tasks = @event.tasks.all
-    end
+  end
+  
+  def show
+    current_event
+    @tasks = @event.tasks.all
+  end
 
-    def destroy
-      current_event
-      @name = @event.name
-      destroy_current_event_tasks
-      @event.destroy
-      respond_to do |format|
-        format.html { redirect_to @event, alert: "Event #{@name} was successfully deleted." }
+  def destroy
+    current_event
+    @name = @event.name
+    destroy_current_event_tasks
+    @event.destroy
+    respond_to do |format|
+      format.html { redirect_to @event, alert: "Event #{@name} was successfully deleted." }
+    end
+  end
+  
+  def destroy_current_event_tasks
+    @tasks = current_event.tasks.all
+    @tasks.each do |task|
+      @expenses = task.expenses.all
+      @expenses.each do |expense|
+        expense.destroy
       end
+      task.destroy
     end
-    
-    def destroy_current_event_tasks
-      @tasks = current_event.tasks.all
-      @tasks.each do |task|
-        task.destroy
-      end
-    end
+  end
 
-    def current_event
-      @event = Event.find(params[:id])
-    end
+  def current_event
+    @event = Event.find(params[:id])
+  end
 
-    def event_params 
-      params.require(:event).permit(:name)
-    end
+  def event_params 
+    params.require(:event).permit(:name)
+  end
 
 end

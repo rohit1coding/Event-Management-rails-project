564a07525c5322a9663bbc429c0deef10bf554a8
rohit1coding <rohit1coding@gmail.com>
2022-07-13 22:32:18 +0530

    signup page user name validation changed

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
   has_secure_password
   
   validates :name, :email, :password, presence: true
-  validates :name, presence: true, format: { with: /\A[a-zA-Z]+\z/, message: "only allows letters" }
+  validates :name, presence: true, format: { with: /\A[a-zA-Z]+\z/, with:/\s/, message: "enter full name with space" }
   validates :email, presence: true, uniqueness: true
   validates :password, presence: true, length: { minimum: 5, message: "Password must be of minimum 5 letters" }
   
